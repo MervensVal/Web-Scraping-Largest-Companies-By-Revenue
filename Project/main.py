fcef41c29947464c4e18739146f1adc6c24b7ec5
Mervens Val <valmervens@gmail.com>
2024-12-22 06:15:23 -0500

    insert data into cvs file

--- Project/main.py
+++ Project/main.py
@@ -4,6 +4,7 @@ import scrape
 try:
     print("Program started...") 
     scrape.pull_revenue_data()
+    print("program ended.")
 except Exception as e:
     print("Error occured during program execution: ", e)
     sys.exit()
--- Project/scrape.py
+++ Project/scrape.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import secret
+import datetime
+
+folder_path = secret.folder_path
 
 def pull_revenue_data():
     #make request and pull table
@@ -14,7 +17,7 @@ def pull_revenue_data():
     world_titles = table.find_all('th')
     table_titles_list = [title.text.strip() for title in world_titles]
 
-    #loop through table rows (tr), find td tags for each row, get inividual data text, and clean text
+    #loop through table rows (tr), find td tags for each row, get inividual data text, clean text, and insert
     df = pd.DataFrame(columns = table_titles_list)
     table_rows = table.find_all('tr') 
     for row in table_rows[1:]:
@@ -22,4 +25,10 @@ def pull_revenue_data():
         individual_row_data = [data.text.strip() for data in row_data]
         length = len(df)
         df.loc[length] = individual_row_data
-    print(df)
+    
+    #insert dataframe into into csv file
+    current_time_str = datetime.datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
+    revenue_file_path = folder_path + '\\Revenue Data\\Largest_Company_Revenue'+current_time_str+'.csv'
+    df.to_csv(revenue_file_path, index=False)
+
+    #insert dataframe into SQL Server Database
